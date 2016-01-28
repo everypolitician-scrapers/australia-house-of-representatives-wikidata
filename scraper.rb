ba047816d675e266ef2acc987c46f1890841362c
Tony Bowden <tony@tmtm.com>
2016-01-28 15:10:18 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,37 +1,8 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-
-require 'nokogiri'
-require 'colorize'
 require 'pry'
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
-
-def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
-end
-
-def wikinames_from(url)
-  noko = noko_for(url)
-  names = noko.xpath('//table[.//th[contains(., "Electorate")]]//tr[td]//td[1]//a[not(@class="new")]/@title').map(&:text)
-  raise "No names found in #{url}" if names.count.zero?
-  return names
-end
-
-def fetch_info(names)
-  WikiData.ids_from_pages('en', names.flatten.compact.uniq).each do |name, id|
-    data = WikiData::Fetcher.new(id: id).data rescue nil
-    unless data
-      warn "No data for #{p}"
-      next
-    end
-    data[:original_wikiname] = name
-    ScraperWiki.save_sqlite([:id], data)
-  end
-end
 
 urls = [
   'Members_of_the_Australian_House_of_Representatives,_2013–2016',
@@ -46,10 +17,13 @@ urls = [
   'Members_of_the_Australian_House_of_Representatives,_1987–1990',
 ]
 
+names = urls.map do |url|
+  EveryPolitician::Wikidata.wikipedia_xpath( 
+    url: "https://en.wikipedia.org/wiki/#{url}",
+    xpath: '//table[.//th[contains(., "Electorate")]]//tr[td]//td[1]//a[not(@class="new")]/@title',
+  ) 
+end.flatten.compact.uniq
 
-fetch_info(
-  urls.map { |u| wikinames_from('https://en.wikipedia.org/wiki/' + URI.encode(u)) }.reduce(&:+)
-)
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+warn EveryPolitician::Wikidata.notify_rebuilder
 
-require 'rest-client'
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
